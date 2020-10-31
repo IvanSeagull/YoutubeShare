b3193bbf6109cf95f44a72b1d0ce4a8c6fa7d60d
Ma10y <significantdeveloper@gmail.com>
2020-11-01 02:05:45 +0300

    a

--- extension/share.js
+++ extension/share.js
@@ -1,3 +1,4 @@
+
 let a = ''
 let button = document.getElementById("share_button")
 const b = 'watch?v='
@@ -12,6 +13,27 @@ function edit (){
   if (a.includes(b)) {
     let d = (a.replace(b,c))
     alert(d)
+    getData()
   }
 }
 
+
+function getData (){
+  
+  var data = {} //тут у нас будет результат
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'http://192.168.1.71:8080/dataaa.json' , false);
+  
+  xhr.send();
+  
+  if (xhr.status != 200) {
+    // обработать ошибку
+    alert( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
+  } else {
+    // вывести результат
+    
+    
+    data = xhr.responseText;
+    alert (data)
+  }
+    }
